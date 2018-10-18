14e5b33d5824354ba06f9e98fdccf7e0e6726d02
Filip <filipcoja@gmail.com>
2018-10-18 20:09:09 +0200

    [TASK] Added Colorpicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { SliderComponent } from './slider/slider.component';
+import { ColorpickerComponent } from './colorpicker/colorpicker.component';
+import { EditorComponent } from './editor/editor.component';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSliderModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    ColorpickerComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
